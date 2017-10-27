1aecdafc8eb126d0f38b0c62b5c7f7041fb39d96
Kasun Kodagoda <kvkrusl@gmail.com>
2017-10-28 01:30:52 +0530

    Finished the initial implementation of the task.

--- BuildTask/RegexMatchReplace/regexMatchReplace.ts
+++ BuildTask/RegexMatchReplace/regexMatchReplace.ts
@@ -4,14 +4,37 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 
-task.setResourcePath(path.join(__dirname, 'task.json'));
+Task.setResourcePath(path.join(__dirname, 'task.json'));
 
-async function run(): void {
+async function run(): Promise<void> {
     const filePath: string = Task.getPathInput('PathToFile', true);
     const regExString: string = Task.getInput('RegEx', true);
     const valueToReplace: string = Task.getInput('ValueToReplace', true);
 
+    Task.debug(`File path: ${filePath}`);
+    Task.debug(`Regular Expression: ${regExString}`);
+    Task.debug(`Replacement Value: ${valueToReplace}`);
+
     const regEx: RegExp = new RegExp(regExString, 'g');
+
+    fs.readFile(filePath, 'utf8', (readError, data) => {
+        if (readError) {
+            Task.setResult(Task.TaskResult.Failed, `Failed to read the file. File path: ${filePath}`);
+            return;
+        }
+
+        const modifiedContent = data.replace(regEx, valueToReplace);
+
+        fs.writeFile(filePath, modifiedContent, 'utf8', writeError => {
+            if (writeError) {
+                Task.setResult(Task.TaskResult.Failed, `Failed to write the file. Error: ${writeError.message}`);
+                return;
+            }
+
+            Task.setResult(Task.TaskResult.Succeeded, 'RegEx Match Replace Complete.');
+        });
+    });
+
 }
 
 run();
